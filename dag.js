fd855b48ffeaba7657605bde2bed5aa8fe29d36d
Robert Martin <rdm@google.com>
2013-06-17 14:32:44 -0400

    update to show simple DAG graph.
    Ugly code, but somewhat useful viz.

--- dag.js
+++ dag.js
@@ -1,43 +1,12 @@
 
+
 // Generate DAG
 var nodeCount = 8;
 var svgWidth = 900, svgHeight = 300;
-
-/*
-// example: [[false, true, true], [false false true], ...]
-var dagMatrix = d3.range(nodeCount).map(function(d1, i1) {
-  return d3.range(nodeCount).map(function(d2, i2) {
-    return !!(i1 - i2 > 0 && Math.random() > 0.6);
-  });
-});
-
-// example: [[1, 3, 4], [2], [4], ...]
-var adjacencyList = dagMatrix.map(function(matrixRow) {
-  var edges = [];
-  matrixRow.forEach(function(d, i) {
-    if (d) { edges.push(i); }
-  });
-  return edges;
-});
-
-// example: [{x, y, value}, {x, y, value}, ...]
-var dagMatrixCells = dagMatrix.map(function(row, rowIndex) {
-  return row.map(function(cell, colIndex) {
-    return { x: rowIndex, y: colIndex, value: cell };
-  });
-}).reduce(function(a, b) { return a.concat(b); });
-
-// example: [{x, y, value}, {x, y, value}, ...]
-var dagMatrixCells = links.map(function(row, rowIndex) {
-  return row.map(function(cell, colIndex) {
-    return { x: rowIndex, y: colIndex, value: cell };
-  });
-}).reduce(function(a, b) { return a.concat(b); });
-
-*/
-
 // The nodes are indexed by topological sort.
-var nodes = d3.range(nodeCount).map(function(d) { return { index: d }; });
+var nodes = d3.range(nodeCount).map(function(d) {
+  return { index: d, label: 'node ' + d };
+});
 var links = d3.range(nodeCount * nodeCount).map(function(i) {
   return {
     source: Math.floor(i / nodeCount),
@@ -75,17 +44,39 @@ var dagMatrixCells = d3.merge(d3.range(nodeCount).map(function(rowIndex) {
   });
 }));
 
+
+//
+// Create d3 Components.
+//
+
 // This is the d3 component that facilitates the force graph.
 var force = d3.layout.force()
     .nodes(nodes)
     .links(links)
     .size([svgWidth, svgHeight])
     .linkDistance(30 * Math.sqrt(nodeCount))
+    .linkStrength(0.5)
     .charge(-300);
 // Run the simulation a few times to settle things down.
 force.start();
 for (var i = 0; i < 30; i++) { force.tick(); }
 force.stop();
+
+// Create a separate set of node objects for DAG, so the index values don't collide.
+var dagNodes = d3.range(nodeCount).map(function(d) {
+  return { index: d, label: 'node ' + d };
+});
+var dagLinks = links.map(function(d) {
+  return { source: dagNodes[d.source.index], target: dagNodes[d.target.index] };
+});
+var dag = new dagLayout(dagNodes, dagLinks, svgWidth, svgHeight);
+/*
+var dag = d3.layout.dag()
+    .nodes(nodes)
+    .links(links)
+    .size([svgWidth, svgHeight])
+    .
+    */
 
 //
 // Create SVG elements.
@@ -101,14 +92,14 @@ var svg = d3.select('#force')
 svg.append('defs').append('marker')
     .attr({
       id: 'arrowhead',
-      refX: 10,
+      refX: 13,
       refY: 4,
       markerUnits: 'strokeWidth',
-      markerWidth: 300,
-      markerHeight: 400,
+      markerWidth: 10,
+      markerHeight: 8,
       orient: 'auto'
     }).append('path').attr({
-      d: 'M 0 0 L 10 4 L 0 8 z',
+      d: 'M 0 0 L 10 4 L 0 8 Z',
       fill: 'red'
     });
 
@@ -141,9 +132,41 @@ var svgNodes = svg.selectAll('.node')
 svgNodes.append('circle')
     .attr('r', 5);
 svgNodes.append('text')
-    .attr('transform', 'translate(-6, 20)')
-    .text(function(d, i) { return 'node ' + i; });
+    .attr('transform', 'translate(-6,20)')
+    .text(function(d) { return d.label; });
 
+// Draw the DAG in its own SVG.
+svg = d3.select('#dag')
+    .append('svg')
+    .attr({
+        width: svgWidth,
+        height: svgHeight,
+        xmlns: 'http://www.w3.org/2000/svg'
+      });
+var svgDagEdges = svg.selectAll('.daglink')
+    .data(dagLinks)
+    .enter().append('line')
+        .attr('class', 'daglink')
+        .style('stroke', 'red')
+        .attr('marker-end', 'url(#arrowhead)')
+        .attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+
+var svgDagNodes = svg.selectAll('.dagnode')
+    .data(dagNodes)
+    .enter().append('g')
+        .attr('class', 'dagnode')
+        .attr('transform', function(d) {
+          return 'translate(' + d.x + ',' + d.y + ')';
+        });
+svgDagNodes.append('circle').attr('r', 5);
+svgDagNodes.append('text')
+    .attr('transform', 'translate(-6,20)')
+    .text(function(d) { return d.label; });
+
+// The tick function will update the force graph every tick.
 function tick() {
   svgEdges
       .attr("x1", function(d) { return d.source.x; })
@@ -156,10 +179,82 @@ function tick() {
         return 'translate(' + d.x + ',' + d.y + ')';
       });
 }
-
 // Everything is ready. Start the simulation!
 force.start()
     .on('tick', tick);
 
 
 
+// Class for DAG layout.
+function dagLayout(nodes, links, svgWidth, svgHeight) {
+  // For now, assume nodes are topologically sorted (they are).
+
+
+  // Create reference arrays to parents and children.
+  nodes.forEach(function(node) {
+    node.children = [];
+    node.parents = [];
+  });
+
+  links.forEach(function(link) {
+    var sourceNode = link.source
+    var targetNode = link.target;
+    sourceNode.children.push(targetNode);
+    targetNode.parents.push(sourceNode);
+  });
+
+  // Calculate X values.
+  nodes.forEach(function(node) {
+    node.x = d3.max(node.parents, function(node) { return node.x; }) + 1 || 0;
+  });
+  nodes.forEach(function(node) {
+    var maxX = d3.min(node.children, function(node) { return node.x; }) - 1;
+    node.x = maxX ? maxX : node.x;
+  });
+  nodes.sort(function(a, b) { return a.x - b.x; });
+
+  // Stateful function to remember which spots are "taken".
+  var nearestY = (function() {
+    var taken = {};
+    return function(x, y) {
+      taken[x] = taken[x] || {};
+      var oldY = y;
+      while (taken[x][y]) {
+        y += 1;
+      }
+      taken[x][y] = true;
+      // console.log('nearestY(' + x + ',' + oldY + ') -> ' + y);
+      return y;
+    };
+  })();
+
+  var onlyChild = function(node) {
+    return node.parents.length == 1 && node.parents[0].children.length == 1;
+  };
+  var placementIndex = 0;
+  nodes.forEach(function(node) {
+    if (node.parents.length == 0) {
+      node.y = nearestY(0, 0);
+      node.label += ' (' + placementIndex++ + ')';
+    } else if (onlyChild(node)) {
+      // Skip. This node has already been placed.
+    } else {
+      var maxX = d3.max(node.parents, function(p) { return p.x; });
+      var avgY = Math.floor(d3.mean(node.parents, function(p) { return p.y; }));
+      node.y = nearestY(node.x, avgY);
+      node.label += ' (' + placementIndex++ + ')';
+    }
+
+    while (node.children.length == 1 && onlyChild(node.children[0])) {
+      var child = node.children[0];
+      child.y = nearestY(child.x, node.y);
+      node = child;
+      node.label += ' +(' + placementIndex++ + ')';
+    }
+  });
+
+  nodes.forEach(function(node) {
+    node.x = node.x * 100 + 50;
+    node.y = node.y * 50 + 50;
+  })
+};
